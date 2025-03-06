1b03ba8b74bb50a04114c486d82618b0067ba160
Peter Sliwka <piotrsliwka333@gmail.com>
2025-03-06 03:37:27 +0100

    Master - Update react router configuration in App.tsx file

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router";
+import { Layout } from "../components/Shared/Layout/Layout";
+import { NotFound } from "../pages/NotFound/NotFound";
+import { Workouts } from "../pages/Workouts/Workouts";
 import { Home } from "../pages/Home/Home";
-import { Layout } from "../components/Layout/Layout";
 
 export const App: React.FC = () => {
   return (
@@ -9,6 +11,11 @@ export const App: React.FC = () => {
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
+          <Route path="/workouts">
+            <Route path="" element={<Workouts />} />
+          </Route>
+
+          <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
     </BrowserRouter>
