68070f9bf7a152e6354c9fd575f3856cad00b9c8
Peter Sliwka <piotrsliwka333@gmail.com>
2025-03-06 03:42:00 +0100

    Master - Create Workouts page

---
+++ src/pages/Workouts/Workouts.tsx
@@ -0,0 +1,7 @@
+export const Workouts: React.FC = () => {
+  return (
+    <section className="rounded-md border border-inactive p-4 bg-white">
+      <h1 className="text-center text-xl">Workouts</h1>
+    </section>
+  );
+};
