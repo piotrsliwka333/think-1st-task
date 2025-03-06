acf571ccf5ec8ed66b726e9ffccff515b424c171
Peter Sliwka <piotrsliwka333@gmail.com>
2025-03-06 03:34:33 +0100

    Master - Create Slider component

---
+++ src/components/Shared/Slider/Slider.tsx
@@ -0,0 +1,62 @@
+export const Slider: React.FC<OwnProps> = (props) => {
+  const { label, value, onChange, id, name, className = "", max, min } = props;
+
+  if (value < min) throw new Error("Adjust min value");
+  return (
+    <div className={`${className} relative w-full`}>
+      <label htmlFor={id} className="block text-md mb-4">
+        {label}
+      </label>
+
+      <div className={`relative`}>
+        <input
+          max={max}
+          min={min}
+          className={`slider w-full h-1 bg-inactive rounded-lg appearance-none cursor-pointer focus:outline-none range-sm`}
+          type="range"
+          name={name}
+          id={id}
+          value={value}
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+            onChange(event)
+          }
+        />
+
+        <div
+          className={`block absolute  bg-primary h-1  start-0 bottom-[5px] `}
+          style={{ width: `${((value - min) * 100) / (max - min)}%` }}
+        ></div>
+
+        {/* here we need to adjust that the entire
+              100% of track-field for value helper text
+              indicator will be smaller about 20px 
+              because size of this counter is 20px
+          */}
+        <div className="relative w-[calc(100%-20px)] mt-1">
+          <span
+            className="tooltip text-sm absolute text-primary bg-white rounded-md flex items-center justify-center start-0 -bottom-8 w-[40px] h-[25px] border border-inactive translate-x-[-25%]"
+            style={{ left: `${((value - min) * 100) / (max - min)}%` }}
+          >
+            {value}
+          </span>
+        </div>
+
+        <span className="text-sm absolute start-0 bottom-4 translate-x-[50%]">
+          {min}
+        </span>
+        <span className="text-sm absolute end-0 bottom-4">{max}</span>
+      </div>
+    </div>
+  );
+};
+
+interface OwnProps {
+  className?: string;
+  id: string;
+  label: string;
+  name: string;
+  value: number;
+  max: number;
+  min: number;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
