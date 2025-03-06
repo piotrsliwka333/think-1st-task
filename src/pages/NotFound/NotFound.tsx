cf1a04dd7645b84b7f7d280b664f96bdd2e7776c
Peter Sliwka <piotrsliwka333@gmail.com>
2025-03-06 03:41:43 +0100

    Master - Create NotFound page

---
+++ src/pages/NotFound/NotFound.tsx
@@ -0,0 +1,7 @@
+export const NotFound: React.FC = () => {
+  return (
+    <section className="rounded-md border border-inactive p-4 bg-white">
+      <h1 className="text-center text-xl">Page Not Found</h1>
+    </section>
+  );
+};
